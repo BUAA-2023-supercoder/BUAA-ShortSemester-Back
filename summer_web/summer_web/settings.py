45e17f36d1928a5a5fe0d5479e89f68a4d496afd
Abyss <abyss7893@163.com>
2023-08-25 17:35:26 +0800

    add urls about media

--- summer_web/summer_web/settings.py
+++ summer_web/summer_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,6 +26,9 @@ SECRET_KEY = 'django-insecure-6moinmdbtln4agwwvz#*)*!d&bw^z1a^lk4$ca9@(&b*^v9m(z
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Application definition
 
--- summer_web/summer_web/urls.py
+++ summer_web/summer_web/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from summer_web import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,5 @@ urlpatterns = [
     path("api/", include("TeamApi.urls")),
     path("api/", include("ProjectApi.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
