c2ff792c0dd719d7294934c40b2452567244aefc
chenyuxin <2519386018@qq.com>
2023-08-25 10:48:27 +0800

    setadmin

--- summer_web/TeamApi/views.py
+++ summer_web/TeamApi/views.py
@@ -27,7 +27,29 @@ def setAdmin(request):
     if request.method != "POST":
         return JsonResponse({'msg': 'fail', 'error': 'wrong request method'}, status=500)
     data = json.loads(request.body)
-
+    accessToken = request.headers.get('Authorization').split(' ')[1]
+    decodedToken = validateAccessToken(accessToken)
+    teamId = data.get('teamID')
+    email = data.get('email')
+    newrole=data.get('role')
+    if decodedToken:
+        try:
+            team = Team.objects.get(id=teamId)  # team
+            user = UserInfo.objects.get(email=email)  # user
+            ordinarymember = TeamMember.objects.get(member=user, teamID=team)
+            admin = UserInfo.objects.get(email=getUserFromToken(accessToken))  # user
+            #判断管理员是否有管理权限
+            # 判断邀请者的权限
+            team_member = TeamMember.objects.get(member=admin, teamID=team)
+            role = team_member.role
+            if role == 2:
+                return JsonResponse({'message': '成员权限不足'}, status=400)
+            TeamMember.objects.get(member=ordinarymember,teamID=team).update(role=newrole)
+            return JsonResponse({'message': 'success'}, status=200)
+        except UserInfo.DoesNotExist:
+            return JsonResponse({'message': '成员不存在'}, status=400)
+        except Team.DoesNotExist:
+            return JsonResponse({'message': '团队不存在'}, status=400)
 def invite(request):
     if request.method != "POST":
         return JsonResponse({'msg': 'fail', 'error': 'wrong request method'}, status=500)
@@ -43,9 +65,7 @@ def invite(request):
             team = Team.objects.get(id=teamId)  # team
             invitees = UserInfo.objects.get(email=email)    # user
             # 判断邀请者的权限
-            print(inviter)
             team_member = TeamMember.objects.get(member=inviter, teamID=team)
-            print(inviter)
             role = team_member.role
             if role == 2:
                 return JsonResponse({'message': '成员权限不足'}, status=400)
