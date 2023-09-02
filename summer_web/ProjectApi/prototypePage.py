77e6b995bb2ff4095098280f8115de6c6b1e50a4
Abyss <abyss7893@163.com>
2023-09-02 22:02:21 +0800

    fix page

--- summer_web/ProjectApi/prototypePage.py
+++ summer_web/ProjectApi/prototypePage.py
@@ -13,28 +13,21 @@ def getPage(request, pageID):
     if request.method != "GET":
         return JsonResponse({'msg': 'fail', 'error': 'wrong request method'}, status=500)
 
-    accessToken = request.headers.get('Authorization').split(' ')[1]
-    if validateAccessToken(accessToken):
-        page = PrototypePage.objects.get(id=pageID)
-        user = UserInfo.objects.get(email=getUserFromToken(accessToken))
-        if page is None:
-            return JsonResponse({'msg': 'fail', 'error': 'pageID is wrong'}, status=400)
-        if TeamMember.objects.filter(member=user, teamID=page.project.team).count() == 0:
-            return JsonResponse({'msg': 'fail', 'error': 'you should not access this page'}, status=400)
-        info = {
-            'onEdit': page.onEdit,
-            'context': page.context,
-            'ID': page.id,
-            'height': page.height,
-            'width': page.width,
-            'name': page.prototypeName,
-            'lastEditTime': page.lastEditTime.strftime("%Y-%m-%d %H:%M:%S"),
-            'lastEditPerson': page.lastEditPerson,
-            'projectID': page.project.id
-        }
-        return JsonResponse({'msg': 'success', 'info': info}, status=200)
-    else:
-        return JsonResponse({'msg': 'fail', 'error': 'user does not exist'}, status=400)
+    page = PrototypePage.objects.get(id=pageID)
+    if page is None:
+        return JsonResponse({'msg': 'fail', 'error': 'pageID is wrong'}, status=400)
+    info = {
+        'onEdit': page.onEdit,
+        'context': page.context,
+        'ID': page.id,
+        'height': page.height,
+        'width': page.width,
+        'name': page.prototypeName,
+        'lastEditTime': page.lastEditTime.strftime("%Y-%m-%d %H:%M:%S"),
+        'lastEditPerson': page.lastEditPerson,
+        'projectID': page.project.id
+    }
+    return JsonResponse({'msg': 'success', 'info': info}, status=200)
 
 
 def savePage(request):
